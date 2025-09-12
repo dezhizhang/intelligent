6899644b187b38025a34be73de6def3b3f5d9e7e
dezhizhang <1541609448@qq.com>
2025-09-12 20:41:26 +0800

    feat:chapter

---
+++ numpy/8chapter/main.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+
+# num = np.array([1,'hello'])
+# print(num)
+
+# arr = np.array(1)
+# print(arr)
+
+# arr = np.array(1)
+# print(arr)
+#
+# # 查看数组的形状
+# print(arr.shape)
+
+
+# arr = np.array([1,2,3])
+# print(arr)
+#
+# print(arr.shape)
+#
+# # 元数的个数
+# print(arr.size)
+
+
+arr = np.array([1,2,3,4])
+print(arr.T)
+# print(arr)
+
+
+
+
+
