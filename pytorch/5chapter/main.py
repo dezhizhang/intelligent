de1e73922f8eff092762fde0bcfdc9d5c14ff6bc
dezhizhang <1541609448@qq.com>
2025-09-16 07:22:35 +0800

    feat:第五章

---
+++ pytorch/5chapter/main.py
@@ -0,0 +1,17 @@
+# import torch
+#
+# dev = torch.device("cpu")
+# a = torch.tensor([2,2],dtype=torch.float64,device=dev)
+#
+# print(a)
+
+import torch
+
+dev = torch.device("cpu")
+
+i = torch.tensor([[0,1,2],[0,1,2]])
+v = torch.tensor([1,2,3])
+a = torch.sparse_coo_tensor(i,v,(4,4)).to_sparse()
+
+print(a)
+
