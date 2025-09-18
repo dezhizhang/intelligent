f8320db4cafba14c11da37a813daba9b353d5a4d
dezhizhang <1541609448@qq.com>
2025-09-18 08:32:22 +0800

    feat:聊天模型

---
+++ langchain/16chapter/main.py
@@ -0,0 +1,23 @@
+
+
+# from langchain_openai import ChatOpenAI
+#
+# llm = ChatOpenAI(
+#     model="gpt-4o-mini",
+#     api_key="sk-ifGCp8P1b7mgq2AiJxLDDkZcCjF4KoukEEhzVfpzPufSBJQs",
+#     base_url="https://poloai.top/v1"
+# )
+#
+# response = llm.invoke("你好，贵州数擎科技有限公司")
+# print(response.content)
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    api_key="sk-ifGCp8P1b7mgq2AiJxLDDkZcCjF4KoukEEhzVfpzPufSBJQs",
+    base_url="https://poloai.top/v1"
+)
+
+response = llm.invoke("贵州数擎科技有限公司")
+print(response.content)
