2c70528026096376e43c2d93699a3981dfc039be
dezhizhang <1541609448@qq.com>
2025-09-27 17:22:46 +0800

    feat: 张量

---
+++ lpytorch/1chapter/main.py
@@ -0,0 +1,64 @@
+
+# import torch
+# import numpy as np
+#
+# # 创建标是张量
+# tensor1 = torch.tensor(10)
+# print(tensor1)
+#
+# tensor2 = torch.tensor([1,2,3])
+# print(tensor2)
+#
+# # 使用npmpy创建张量
+# tensor3 = torch.tensor(np.array())
+
+# import torch
+# import numpy as np
+#
+# # 创建张量
+# tensor1 = torch.tensor(10)
+# print(tensor1)
+
+# import torch
+# import numpy as np
+#
+# tensor1 = torch.tensor(10)
+#
+# print(tensor1)
+#
+# torch.tensor(10)
+# torch.Size([])
+
+# import torch
+#
+# tensor2 = torch.tensor([1,2,3])
+# print(tensor2)
+# print(tensor2.size())
+# print(tensor2.dtype)
+
+# import torch
+# import numpy as np
+#
+# tensor = torch.tensor(np.array([[1,2,3],[4,5,6]]))
+# print(tensor)
+# print(tensor.size())
+# print(tensor.dtype)
+
+# import torch
+#
+# tensor = torch.Tensor(3,2,4)
+# print(tensor)
+# print(tensor.size())
+# print(tensor.dtype)
+
+
+
+
+
+
+
+
+
+
+
+
