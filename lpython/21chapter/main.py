b62af17e56e6c181da8cd154d7f40dc70d022ed1
dezhizhang <1541609448@qq.com>
2025-09-27 11:09:05 +0800

    feat: 字符串拼接

--- lpython/21chapter/main.py
+++ lpython/21chapter/main.py
@@ -1,3 +1,25 @@
-name = "数擎科技有限公司"
+# name = "数擎科技有限公司"
+# print(name)
+#
+
+
+# name = '数擎科技有限公司'
+# print(type(name))
+
+# name = """
+# 数擎科技有限公司
+# """
+#
+# print(type(name))
+
+# name = '"数擎科技有限公司"'
+# print(name)
+
+name = '"数擎科技有限公司"'
+
 print(name)
+
+
+
+
 
