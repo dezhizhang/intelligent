c10e9b966bf6e759ee4536fc3d1d151520d92638
dezhizhang <1541609448@qq.com>
2025-09-27 11:40:32 +0800

    feat: 精度控制

---
+++ lpython/22chapter/main.py
@@ -0,0 +1,14 @@
+
+
+# name1 = "贵州"
+# name2 = "数擎科技有限公司"
+#
+# print(name1 + name2)
+
+name = "tom"
+phone = 15992478448
+
+print("姓名:" + name + str(phone))
+
+
+
---
+++ lpython/23chapter/main.py
@@ -0,0 +1,43 @@
+#
+# name = "有限公司"
+# company = "贵州数擎科技%s" % name
+# print(company)
+
+
+
+
+# class_name = 57
+# avg_salary = 16781
+#
+# message = "人工智能 贵州%s期, 平均工资%s" % (class_name,avg_salary)
+# print(message)
+
+
+# name = "贵州数擎科技有限公司"
+# setup_year = 2024
+# stock_price = 19.99
+#
+# message = "%s,成立于%d 股价%f" % (name,setup_year,stock_price)
+# print(message)
+#
+# name = "贵州数擎科技有限公司"
+# setup_year = 2024
+# stock_price = 19.99
+#
+# message = "%s 成立于%d 股价%f" % (name,setup_year,stock_price)
+# print(message)
+
+name = "贵州数擎科技有限公司"
+setup_year = 2024
+stock_price = 19.99
+
+message = "%s 成立于%d 股价%f" % (name,setup_year,stock_price)
+print(message)
+
+
+
+
+
+
+
+
