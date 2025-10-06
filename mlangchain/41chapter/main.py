300379b0bbc0c3c65a164335d11617203065aa47
dezhizhang <1541609448@qq.com>
2025-10-06 09:22:25 +0800

    feat:聊天链

--- mlangchain/41chapter/main.py
+++ mlangchain/41chapter/main.py
@@ -1,21 +1,22 @@
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
+from langchain.chains.llm import  LLMChain
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_ollama import ChatOllama
 
 llm = ChatOllama(
     model="deepseek-r1:7b"
 )
 
-
-prompt = PromptTemplate.from_template(
-    template="你是一位数学高手,帮我解决如下的数学问题：{question}"
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","你是一个数学高手"),
+        ("human","帮我解决如下的数学问题:{question}")
+    ]
 )
 
-chain = LLMChain(llm=llm,prompt=prompt)
+chain = LLMChain(llm = llm,prompt=prompt,verbose=True)
 
-response = chain.invoke("1 + 2 * 3 = ？")
+response = chain.invoke(input={"question":"1 + 2 * 3 = ?"})
 print(response)
-
 
 
 
--- mlangchain/READEME.md
+++ mlangchain/READEME.md
@@ -236,3 +236,28 @@ print(response)
 
 
 ```
+
+### ChatPromptTemplate
+```python
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model="deepseek-r1:7b"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你是一个数学高手"),
+        ("human", "帮我解决如下的数学问题:{question}")
+    ]
+)
+
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+response = chain.invoke(input={"question": "1 + 2 * 3 = ?"})
+print(response)
+
+
+```
