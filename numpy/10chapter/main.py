493ba2517b420c00b3e08ccea908060b44d9b18a
dezhizhang <1541609448@qq.com>
2025-10-12 09:36:15 +0800

    feat: 数列的调用

--- numpy/10chapter/main.py
+++ numpy/10chapter/main.py
@@ -1,4 +1,30 @@
 
+# import  numpy as np
+#
+# arr = np.logspace(0,4,3,base=2)
+# print(arr)
+
+# 等间隔数列
+# import numpy as np
+#
+# arr = np.linspace(0,100,5)
+# print(arr)
+
+# 对数间隔数列
+import numpy as np
+
+arr = np.linspace(0,100,5)
+print(arr)
+
+
+
+
+
+
+
+
+
+
 
 
 
