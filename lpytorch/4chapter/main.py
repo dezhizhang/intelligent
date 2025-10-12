696ed727487964befee865d2175d86693bb71e0e
dezhizhang <1541609448@qq.com>
2025-10-12 20:12:21 +0800

    feat:算数运算

---
+++ lpytorch/4chapter/main.py
@@ -0,0 +1,105 @@
+# import torch
+import torch
+
+# 加法运算
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# # print(a)
+# # print(b)
+#
+# print(a)
+#
+#
+# # print(a + b)
+# # print(a.add(b))
+
+# 减法运算
+# import torch
+#
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a - b)
+# print(torch.sub(a,b))
+# print(a.sub_(b))
+
+# import torch
+#
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a - b)
+# print(torch.sub(a,b))
+# print(a.sub_(b))
+
+# # 除法
+# import torch
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a / b)
+
+# 除法
+# import  torch
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a / b)
+
+# ## 高维tensor
+# import torch
+# a = torch.ones(1,2,3,4)
+# b = torch.ones(1,2,4,3)
+#
+# print(a.matmul(b).shape)
+
+###
+# import torch
+# a = torch.ones(1,2,3,4)
+# b = torch.ones(1,2,4,3)
+#
+# print(a.matmul(b).shape)
+
+# a = torch.tensor([1,2])
+# print(torch.pow(a,3))
+# print(a.pow(3))
+# print(a**3)
+
+
+# 指数计算
+# a = torch.tensor([1,2],dtype=torch.float32)
+# print(torch.exp(a))
+# print(a.exp())
+
+# a = torch.tensor([1,2],dtype=torch.float32)
+# print(torch.exp(a))
+# print(a.exp())
+
+# log计算
+# a = torch.tensor([10,2],dtype=torch.float32)
+# print(torch.log(a))
+# print(torch.log_(a))
+
+a = torch.tensor([10,2],dtype=torch.float32)
+print(torch.log(a))
+print(torch.log_(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
