22ab1afec959f718f364ecc7cdb522aacf8e8013
dezhizhang <1541609448@qq.com>
2025-10-12 23:04:20 +0800

    feat:tenson的拼接

---
+++ lpytorch/16chapter/main.py
@@ -0,0 +1,41 @@
+# import torch
+#
+# a = torch.zeros((2,4))
+# b = torch.ones((2,4))
+#
+# out = torch.cat((a,b),dim=0)
+# print(out)
+
+# import torch
+#
+# a = torch.zeros((2,4))
+# b = torch.ones((2,4))
+#
+# out = torch.cat((a,b),dim=0)
+# print(out)
+
+
+# import torch
+#
+# a = torch.linspace(1,6,6).view(2,3)
+# b = torch.linspace(7,12,6).view(2,3)
+#
+# out = torch.stack((a,b),dim=0)
+# print(out)
+
+# import torch
+#
+# a = torch.linspace(1,6,6).view(2,3)
+# b = torch.linspace(7,12,6).view(2,3)
+#
+# out = torch.stack((a,b),dim=0)
+#
+# print(out)
+# print(out.shape)
+
+
+
+
+
+
+
