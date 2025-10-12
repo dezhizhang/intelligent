c46f9af945c42b51c2006699a625481d17bad42b
dezhizhang <1541609448@qq.com>
2025-10-12 19:15:59 +0800

    feat:tensor的运算

--- lpytorch/1chapter/main.py
+++ lpytorch/1chapter/main.py
@@ -1,65 +1,99 @@
 
 # import torch
-# import numpy as np
 #
-# # 创建标是张量
-# tensor1 = torch.tensor(10)
-# print(tensor1)
+# a = torch.Tensor([[1,2],[3,4]])
+# print(a)
+# print(a.type())
 #
-# tensor2 = torch.tensor([1,2,3])
-# print(tensor2)
+# a = torch.Tensor(2,3)
+# print(a)
+# print(a.type())
 #
-# # 使用npmpy创建张量
-# tensor3 = torch.tensor(np.array())
+# # 定义全是1的tensor
+#
+# a = torch.ones(2,2)
+# print(a)
+# print(a.type())
 
-# import torch
-# import numpy as np
-#
-# # 创建张量
-# tensor1 = torch.tensor(10)
-# print(tensor1)
+# 定义全是0的张量
+# import  torch
+# a = torch.zeros(2,2)
+# print(a)
+# print(a.type())
 
-# import torch
-# import numpy as np
-#
-# tensor1 = torch.tensor(10)
-#
-# print(tensor1)
-#
-# torch.tensor(10)
-# torch.Size([])
+# import  torch
+# a = torch.zeros(2,2)
+# print(a)
+# print(a.type())
 
+# 对角钱tensor
 # import torch
-#
-# tensor2 = torch.tensor([1,2,3])
-# print(tensor2)
-# print(tensor2.size())
-# print(tensor2.dtype)
-
-# import torch
-# import numpy as np
-#
-# tensor = torch.tensor(np.array([[1,2,3],[4,5,6]]))
-# print(tensor)
-# print(tensor.size())
-# print(tensor.dtype)
+# a = torch.eye(2,2)
+# print(a)
+# print(a.type())
 
 # import torch
 #
-# tensor = torch.Tensor(3,2,4)
-# print(tensor)
-# print(tensor.size())
-# print(tensor.dtype)
+# a = torch.eye(2,2)
+# print(a)
+# print(a.type())
+
+# 随机tensor
+# import  torch
+#
+# a = torch.rand(2,2)
+# print(a)
+# print(a.type())
 
 # import torch
-# import numpy as np
+# a = torch.normal(mean=0.0,std=torch.rand(5))
+# print(a)
+# print(a.type())
+
+# import torch
+# a = torch.normal(mean=torch.rand(5),std=torch.rand(5))
+# print(a)
+
+# 序列
+# import torch
+# a = torch.arange(0,11,3)
+# print(a)
+# print(a.type())
+
+# import torch
 #
-# tensor2 = torch.Tensor(np.array([[1,2,3,4],[4,5,6,7]]))
-# print(tensor2)
-# print(tensor2.dtype)
+# a = torch.arange(0,11,3)
+# print(a)
+# print(a.type())
+
+# import torch
+# a = torch.linspace(2,10,4)
+# print(a)
+# print(a.type())
+
+# import torch
+# a = torch.randperm(10)
+# print(a)
+# print(a.type())
 
 import torch
-print(torch.cuda.is_available())
+a = torch.randperm(10)
+print(a)
+print(a.type())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
