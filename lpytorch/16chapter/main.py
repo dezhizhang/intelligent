46e562016b1c0780fa07cc58dae3256582cee52d
dezhizhang <1541609448@qq.com>
2025-10-13 21:49:40 +0800

    feat:切片

--- lpytorch/16chapter/main.py
+++ lpytorch/16chapter/main.py
@@ -33,6 +33,27 @@
 # print(out)
 # print(out.shape)
 
+# import  torch
+#
+# a = torch.linspace(1,6,6).view(2,3)
+# b = torch.linspace(7,12,6).view(2,3)
+#
+# out = torch.stack((a,b),dim=0)
+# print(out)
+# print(out.shape)
+
+import torch
+a = torch.linspace(1,6,6).view(2,3)
+b = torch.linspace(7,12,6).view(2,3)
+
+out = torch.stack((a,b),dim=0)
+print(out)
+print(out.shape)
+
+
+
+
+
 
 
 
