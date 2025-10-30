951fa816346fa6d6065af558089eb571d8dcbbbe
dezhizhang <1541609448@qq.com>
2025-10-30 23:23:22 +0800

    feat: 对像的构造函数

--- lpython/105chapter/main.py
+++ lpython/105chapter/main.py
@@ -12,11 +12,19 @@
 # ok()
 # hi()
 
-from pkg.module1 import hi
-from pkg.module2 import ok
+# from pkg.module1 import hi
+# from pkg.module2 import ok
+#
+# ok()
+# hi()
 
-ok()
-hi()
+import pkg.module1
+import pkg.module2
+
+pkg.module1.hi()
+pkg.module2.ok()
+
+
 
 
 
---
+++ lpython/108chapter/main.py
@@ -0,0 +1,13 @@
+class Cat:
+    age = None
+    name = None
+    color = None
+
+cat = Cat()
+cat.name = "tom"
+cat.age = 18
+cat.color = "red"
+
+print(f"猫的信息为{cat.name} {cat.age} {cat.color}")
+
+
