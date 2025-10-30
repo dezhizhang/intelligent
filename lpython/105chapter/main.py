cb66b05287f03ecf524012189c40ea683836ffb3
dezhizhang <1541609448@qq.com>
2025-10-30 22:55:12 +0800

    feat: 包的导入

---
+++ lpython/105chapter/main.py
@@ -0,0 +1,32 @@
+
+
+# from pkg.module1 import hi
+# from pkg.module2 import ok
+#
+# ok()
+# hi()
+
+# from pkg.module1 import hi
+# from pkg.module2 import ok
+#
+# ok()
+# hi()
+
+from pkg.module1 import hi
+from pkg.module2 import ok
+
+ok()
+hi()
+
+
+
+
+
+
+
+
+
+
+
+
+
