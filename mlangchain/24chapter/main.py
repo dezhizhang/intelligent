df373a54507722e16c87f94829d9e693685d2f91
dezhizhang <1541609448@qq.com>
2025-11-03 05:56:37 +0800

    feat: 调用提示词模板

--- mlangchain/24chapter/main.py
+++ mlangchain/24chapter/main.py
@@ -1,22 +1,31 @@
 
 
-from langchain.prompts import  PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
+import os
+import dotenv
+dotenv.load_dotenv()
 
 llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
-    base_url="https://poloai.top/v1"
+    model=os.getenv("OPENAI_BASE_MODEL"),
+    base_url=os.getenv("OPENAI_BASE_URL"),
+    api_key=os.getenv("OPENAI_API_KEY")
 )
 
 template = PromptTemplate.from_template(
-    template="请评价{product}的优点,包括{aspect1}"
+    template="请评价{product}的质量,包括{aspect1}和{aspect2}"
 )
 
-prompt = template.invoke(input={"product":"智能手机","aspect1":"拍照质量"})
-
+prompt = template.invoke(input={"product":"智能手机","aspect1":"电池续航","aspect2":"拍照质量"})
 response = llm.invoke(prompt)
+
 print(response)
+
+
+
+
+
 
 
 
