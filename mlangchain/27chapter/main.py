520cd46ba79ca687f055e9681e85aa9b974207e7
dezhizhang <1541609448@qq.com>
2025-11-03 07:23:30 +0800

    feat:MessagesPlaceholder

---
+++ mlangchain/27chapter/main.py
@@ -0,0 +1,21 @@
+# from langchain_core.prompts import ChatPromptTemplate
+#
+# template1 = ChatPromptTemplate.from_messages([
+#     ("system","我是一个人工智能助手，我的名字叫{name}")
+# ])
+#
+# template2 = ChatPromptTemplate.from_messages([
+#     ("human","很高兴认识你，我的问题是{question}")
+# ])
+#
+# prompt_template = ChatPromptTemplate.from_messages([
+#     template1,
+#     template2
+# ])
+#
+# prompt = prompt_template.format_messages(name="晓智",question="1 + 2 * 3 =?")
+#
+# print(prompt)
+
+
+
--- mlangchain/29chapter/main.py
+++ mlangchain/29chapter/main.py
@@ -1,21 +1,61 @@
+#
+#
+# from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+# import os
+# import dotenv
+#
+# dotenv.load_dotenv()
+#
+# llm = ChatOpenAI(
+#     model=os.getenv("OPENAI_BASE_MODEL"),
+#     base_url=os.getenv("OPENAI_BASE_URL"),
+#     api_key=os.getenv("OPENAI_API_KEY"),
+# )
+#
+# # 创建提示词
+# template = ChatPromptTemplate.from_messages([
+#     ("system","你是一个AI助手你的名字叫{name}"),
+#     ("human","我的问题是{question}")
+# ])
+#
+#
+# prompt = template.invoke({"name":"晓智","question":"1 + 2 * 3=?"})
+#
+# response = llm.invoke(prompt)
+# print(response)
+#
 from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.prompts.chat import MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import os
+import dotenv
 
-chat_prompt_template = ChatPromptTemplate.from_messages(
-    [
-        ("system","你是一个AI助手,你的名字是{name}"),
-        MessagesPlaceholder(variable_name="msgs")
-    ]
+dotenv.load_dotenv()
+
+print(os.getenv("OPENAI_BASE_URL"))
+
+llm = ChatOpenAI(
+    model=os.getenv("OPENAI_BASE_MODEL"),
+    api_key=os.getenv("OPENAI_BASE_URL"),
+    base_url=os.getenv("OPENAI_API_KEY")
 )
 
-prompt = chat_prompt_template.invoke({
-    "name":"贵州数擎科技",
-    "msgs":[HumanMessage(content="我的问题是 1 + 2 * 3 = ？")]
+template = ChatPromptTemplate.from_messages([
+    ("system","你是一个AI助手,你的名字叫{name}"),
+    MessagesPlaceholder(variable_name="msgs")
+])
+
+prompt = template.invoke({
+    "name":"晓智",
+    "msgs":[HumanMessage(content="我的问题是1 + 2 * 3 = ?")]
 })
 
 print(prompt)
 
+
+response = llm.invoke(prompt)
+print(response)
 
 
 
