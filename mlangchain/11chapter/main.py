2289f7b0e4f0afd5bd65a995d4cf8f2d26ba3a2c
dezhizhang <1541609448@qq.com>
2025-11-02 17:36:33 +0800

    feat: 对话模型

--- mlangchain/11chapter/main.py
+++ mlangchain/11chapter/main.py
@@ -1,4 +1,4 @@
-from langchain_core.messages import SystemMessage,HumanMessage
+from langchain_core.messages import  SystemMessage,HumanMessage
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
