4a36a57428fc29dc4ad4a30240d3eebfa8ed996e
dezhizhang <1541609448@qq.com>
2025-11-02 14:29:21 +0800

    feat: 联合类型

---
+++ lpython/127chapter/main.py
@@ -0,0 +1,105 @@
+# n1:int = 10
+# n2:float = 10.1
+# is_pass:bool = True
+# name:str = "tom"
+#
+#
+# print(n1)
+# print(n2)
+# print(is_pass)
+# print(name)
+
+# class Cat:
+#     pass
+#
+#
+# cat:Cat = Cat()
+# print(cat)
+
+# my_list:list = [10,20,30]
+# my_tuple:tuple = ("red","green","yellow")
+# my_set:set = {"tom","jack"}
+# my_dict:dict = {"n01":"北京","no2":"上海"}
+#
+# print(my_list)
+# print(my_tuple)
+# print(my_set)
+# print(my_dict)
+
+# def fun1(name:str):
+#     for ele in name:
+#         print(ele)
+#
+# fun1("tom")
+
+# def fun1(name:str):
+#     for ele in name:
+#         print(ele)
+#
+# fun1("jack")
+
+# def fun1(name:str):
+#     for ele in name:
+#         print(ele)
+#
+# fun1("jack")
+#
+
+# def fun2(a:int,b:int) -> int:
+#     return a + b
+#
+# print(fun2(20,10))
+
+# def fun2(a:int,b:int)-> int:
+#     return  a + b
+#
+# print(fun2(10,20))
+
+# def fun2(a:int,b:int) -> int:
+#     return a + b
+#
+# print(fun2(20,30))
+
+# def fun2(a:int,b:int) -> int:
+#     return a + b
+#
+# print(fun2(10.1,20))
+
+# from typing import Union
+# a:Union[int,str] = 100
+#
+# my_list:list[Union[int,str]] = [100,200,300,'tim']
+#
+# print(my_list)
+# print(a)
+
+# from typing import Union
+# a:Union[int,str] = 100
+#
+# my_list:list[Union[int,str]] = [100,200,300,'tim']
+#
+# print(my_list)
+# print(a)
+
+from typing import Union
+a:Union[int,str] = 100
+
+my_list:list[Union[int,str]] = [100,200,300,'tim']
+
+print(my_list)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
