55b51f13bfd0dff59e4e61930ba742739ce85513
dezhizhang <1541609448@qq.com>
2025-11-02 16:57:51 +0800

    feat: 智能体

--- mlangchain/9chapter/main.py
+++ mlangchain/9chapter/main.py
@@ -1,4 +1,47 @@
-from langchain_openai import ChatOpenAI
+# from  langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+#
+# llm = ChatOpenAI(
+#     model="gpt-4",
+#     api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
+#     base_url="https://poloai.top/v1"
+# )
+#
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system","你是世界级的技术文档编写者"),
+#     ("user","{input}")
+# ])
+#
+# chain = prompt | llm
+#
+# message = chain.invoke({"input":"大模型中langchain是什么"})
+# print(message)
+# from  langchain_openai import ChatOpenAI
+# from langchain_core.output_parsers import JsonOutputParser
+#
+# from  langchain_core.prompts import ChatPromptTemplate
+#
+# llm = ChatOpenAI(
+#     model="gpt-4",
+#     api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
+#     base_url="https://poloai.top/v1"
+# )
+#
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system","你是世界级的技术文档编写者"),
+#     ("user","{input}")
+# ])
+#
+# output_parser = JsonOutputParser()
+#
+# chain = prompt | llm | output_parser
+# response = chain.invoke({"input":"langchain是什么? 用json格式回复"})
+# print(response)
+
+
+from langchain_openai import  ChatOpenAI
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 llm = ChatOpenAI(
     model="gpt-4",
@@ -6,8 +49,19 @@ llm = ChatOpenAI(
     base_url="https://poloai.top/v1"
 )
 
-response = llm.invoke("写一首关于冬天的诗")
-print(response.content)
+prompt = ChatPromptTemplate.from_messages([
+    ("system","你是世界级的技术文档编写者"),
+    ("user","{input}")
+])
+
+
+output_parser = JsonOutputParser()
+
+chain = prompt | llm | output_parser
+response = chain.invoke({"input":"langchain是什么？用json格式回复"})
+print(response)
+
+
 
 
 
