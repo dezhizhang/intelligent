ee458317c7c7c54e8a02463655d2b0adf094fcc6
dezhizhang <1541609448@qq.com>
2025-11-04 06:02:17 +0800

    feat: 本地大模型调用

--- mlangchain/39chapter/main.py
+++ mlangchain/39chapter/main.py
@@ -16,19 +16,16 @@
 # print(response.content)
 
 from langchain_core.messages import HumanMessage
-from langchain_ollama import ChatOllama
-
-llm = ChatOllama(
-    model="deepseek-r1:7b"
-)
-
-messages = [
-    HumanMessage(content="介绍一下你自已")
-]
-
-response = llm.invoke(messages)
-
-print(response.content)
+# from langchain_ollama import ChatOllama
+#
+# llm = ChatOllama(
+#     model="deepseek-r1:7b",
+#     base_url="http://localhost:11434"
+# )
+#
+# response = llm.invoke("介绍一下你自已")
+#
+# print(response.content)
 
 
 
