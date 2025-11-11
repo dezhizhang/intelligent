47afa9c6ae433de68d3505571bcd9a79548168c3
dezhizhang <1541609448@qq.com>
2025-11-12 05:55:55 +0800

    feat: 标准化数据据集处理

---
+++ machine/20chapter/main.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import StandardScaler
+
+x_train = [
+    [90, 3, 10, 40],
+    [60, 4, 15, 45],
+    [75, 3, 13, 46]
+]
+
+# 创建标准化对像
+transfer = StandardScaler()
+
+x_train_new = transfer.fit_transform(x_train)
+
+print(f"标准化后的数据集:\n")
+print(x_train_new)
--- machine/README.md
+++ machine/README.md
@@ -1,4 +1,8 @@
 
+# machine 机器学习
+# https://www.bilibili.com/video/BV1Fzszz4Ek7?spm_id_from=333.788.player.switch&vd_source=10257e657caa8b54111087a9329462e8&p=20
+
+### 库安装
 ```python
  pip install scikit-learn
 ```
