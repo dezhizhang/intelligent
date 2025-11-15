117fddb0090701d23a643f9d285beb4a5adb4171
dezhizhang <1541609448@qq.com>
2025-11-16 06:08:29 +0800

    feat: 类的场明

---
+++ lpython/155chapter/main.py
@@ -0,0 +1,20 @@
+# class Car:
+#     pass
+#     def run(self):
+#         print("小汽车在跑!...")
+#
+#
+# car = Car()
+# car.run()
+
+class Car:
+    def run(self):
+        print(f"小汽车在跑!...")
+
+car = Car()
+car.run()
+
+
+
+
+
