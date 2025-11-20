3fbb2b7a1080a9f516782ea38f297a332c5df250
dezhizhang <1541609448@qq.com>
2025-11-21 07:34:07 +0800

    feat: tenser

--- lpytorch/1chapter/main.py
+++ lpytorch/1chapter/main.py
@@ -1,33 +1,25 @@
-
 # import torch
-#
+import torch
+
 # a = torch.Tensor([[1,2],[3,4]])
 # print(a)
 # print(a.type())
+
+# import torch
 #
 # a = torch.Tensor(2,3)
 # print(a)
 # print(a.type())
-#
-# # 定义全是1的tensor
-#
-# a = torch.ones(2,2)
-# print(a)
-# print(a.type())
 
-# 定义全是0的张量
-# import  torch
-# a = torch.zeros(2,2)
-# print(a)
-# print(a.type())
-
-# import  torch
-# a = torch.zeros(2,2)
-# print(a)
-# print(a.type())
-
-# 对角钱tensor
 # import torch
+#
+# a = torch.Tensor(2,3)
+# print(a)
+# print(a.type())
+
+# 对角线
+# import torch
+#
 # a = torch.eye(2,2)
 # print(a)
 # print(a.type())
@@ -38,53 +30,42 @@
 # print(a)
 # print(a.type())
 
-# 随机tensor
-# import  torch
+# 全零
+# a = torch.zeros(2,2)
+# print(a)
+# print(a.type())
+
+# a = torch.zeros(2,2)
+# print(a)
+# print(a.type())
+
+# import torch
+#
+# b = torch.Tensor(2,3)
+# c = torch.zeros_like(b)
+# print(c)
+# print(c.type())
+
+# import torch
+# b = torch.Tensor(2,3)
+# c = torch.zeros_like(b)
+# print(c)
+# print(c.type())
+
+
+# 随机值
+# import torch
 #
 # a = torch.rand(2,2)
 # print(a)
 # print(a.type())
 
-# import torch
-# a = torch.normal(mean=0.0,std=torch.rand(5))
-# print(a)
-# print(a.type())
-
-# import torch
-# a = torch.normal(mean=torch.rand(5),std=torch.rand(5))
-# print(a)
-
-# 序列
-# import torch
-# a = torch.arange(0,11,3)
-# print(a)
-# print(a.type())
-
-# import torch
-#
-# a = torch.arange(0,11,3)
-# print(a)
-# print(a.type())
-
-# import torch
-# a = torch.linspace(2,10,4)
-# print(a)
-# print(a.type())
-
-# import torch
-# a = torch.randperm(10)
-# print(a)
-# print(a.type())
 
 import torch
-a = torch.randperm(10)
+
+a = torch.rand(2,2)
 print(a)
 print(a.type())
-
-
-
-
-
 
 
 
