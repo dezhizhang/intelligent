468fdd07c11ecfaf97a4980bc763b25e30348c0d
dezhizhang <1541609448@qq.com>
2025-11-21 07:45:13 +0800

    feat: 张量的类型

--- lpytorch/1chapter/main.py
+++ lpytorch/1chapter/main.py
@@ -61,11 +61,47 @@ import torch
 # print(a.type())
 
 
-import torch
+# import torch
+#
+# a = torch.rand(2,2)
+# print(a)
+# print(a.type())
 
-a = torch.rand(2,2)
-print(a)
-print(a.type())
+# # 承机序列
+# a = torch.Tensor(2,2).uniform_(-1,1)
+# print(a)
+# print(a.type())
+
+# 序列
+# a = torch.arange(0,11,2)
+# print(a)
+# print(a.type())
+
+# import torch
+# a = torch.arange(0,11,3)
+# print(a)
+
+
+# import torch
+# a = torch.arange(0,11,3)
+# print(a)
+
+
+# import torch
+#
+# a = torch.randperm(10)
+# print(a)
+# print(a.type())
+
+# import numpy as np
+# a = np.array([[1,2],[3,4]])
+# print(a)
+
+
+
+
+
+
 
 
 
