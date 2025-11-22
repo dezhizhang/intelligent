7a97ec119ea757aab2ce6c503fdc1c3fb71a4569
dezhizhang <1541609448@qq.com>
2025-11-22 10:35:41 +0800

    feat: 算数运算

--- lpytorch/4chapter/main.py
+++ lpytorch/4chapter/main.py
@@ -57,13 +57,129 @@ import torch
 
 
 # 除法运算
-import torch
-a = torch.rand(2,3)
-b = torch.rand(2,3)
+# import torch
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a / b)
+# print(torch.mul(a,b))
+# print(a.mul(b))
 
-print(a / b)
-print(torch.mul(a,b))
-print(a.mul(b))
+# import torch
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a / b)
+# print(torch.mul(a,b))
+# print(a.mul(b))
+
+# import torch
+# a = torch.rand(2,3)
+# b = torch.rand(2,3)
+#
+# print(a / b)
+# print(torch.mul(a,b))
+# print(a.mul(b))
+
+# 矩阵运算
+# a = torch.ones(2,1)
+# b = torch.ones(1,2)
+#
+# print(a @ b)
+# print(a.matmul(b))
+# print(torch.matmul(a,b))
+# print(a.mm(b))
+
+# import torch
+# a = torch.ones(2,1)
+# b = torch.ones(1,2)
+#
+# print(a @ b)
+# print(a.matmul(b))
+# print(torch.matmul(a,b))
+# print(a.mm(b))
+
+
+# import torch
+# a = torch.ones(1,2,3,4)
+# b = torch.ones(1,2,4,3)
+#
+# print(a.matmul(b).shape)
+
+# import torch
+#
+# a = torch.ones(1,2,3,4)
+# b = torch.ones(1,2,4,3)
+#
+# print(a.matmul(b))
+
+
+# 指数运算
+# import torch
+# a = torch.tensor([1,2])
+# print(torch.pow(a,3))
+
+# import torch
+# a = torch.tensor([1,2])
+# print(torch.pow(a,3))
+
+# import torch
+# a = torch.tensor([1,2])
+# print(torch.pow(a,3))
+
+# 指数运算
+# import torch
+#
+# a = torch.tensor([1,2],dtype=torch.float32)
+# print(a.type())
+# print(torch.exp(a))
+# print(a.exp())
+# print(a.exp_())
+
+# import torch
+#
+# a = torch.tensor([1,2],dtype=torch.float32)
+# print(a.type())
+# print(torch.exp(a))
+# print(a.exp())
+
+
+# import torch
+# a = torch.tensor([1,2],dtype=torch.float32)
+# print(a.type())
+# print(torch.exp(a))
+# print(a.exp())
+
+# 开平方
+# import torch
+#
+# a = torch.tensor([10,2],dtype=torch.float32)
+# print(torch.sqrt(a))
+# print(torch.sqrt_(a))
+
+import torch
+
+a = torch.tensor([10,2],dtype=torch.float32)
+print(torch.sqrt(a))
+print(torch.sqrt_(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
