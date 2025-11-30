99b8b012c5db5d36b6200c7aed78c8d4ed64f02a
dezhizhang <1541609448@qq.com>
2025-11-30 21:25:01 +0800

    feat: 语义相拟度搜索

--- langgraph/examples/5chaoter/main.py
+++ langgraph/examples/5chaoter/main.py
@@ -1,11 +1,23 @@
-from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI(
-    api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
-    base_url="https://poloai.top/v1",
-    model="gpt-4o-mini",
-    temperature=0.5,
-)
+import chromadb
 
-resp = llm.invoke("hello")
-print(resp)
+def list_collection(db_path:str):
+    client = chromadb.PersistentClient(db_path)
+    collections = client.list_collections()
+    print(f"chromadb:{db_path} 有{len(collections)} 个")
+
+    for i,collection in enumerate(collections):
+        print(f"collection:{i}:{collection.name} 共用{collection.count()} 条记录")
+
+def delete_collection(dbpath,collection_name):
+    try:
+        client = chromadb.PersistentClient(dbpath)
+        client.delete_collection(collection_name)
+    except Exception as e:
+        print(f"删除{collection_name} 时出错 {e}")
+
+
+
+
+db_path = "../3chapter/chroma_langchain_db"
+list_collection(db_path)
