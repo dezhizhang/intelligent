57bd2f3594b49ce0ea5492c7e139658aa9c455d5
dezhizhang <1541609448@qq.com>
2025-12-04 22:50:51 +0800

    feat: 多轮对话

--- mlangchain/9chapter/main.py
+++ mlangchain/9chapter/main.py
@@ -39,28 +39,6 @@
 # print(response)
 
 
-from langchain_openai import  ChatOpenAI
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
-llm = ChatOpenAI(
-    model="gpt-4",
-    api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
-    base_url="https://poloai.top/v1"
-)
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system","你是世界级的技术文档编写者"),
-    ("user","{input}")
-])
-
-
-output_parser = JsonOutputParser()
-
-chain = prompt | llm | output_parser
-response = chain.invoke({"input":"langchain是什么？用json格式回复"})
-print(response)
-
 
 
 
