fd211a2b60f1a5b55452d10ff4230418de31d3a5
dezhizhang <1541609448@qq.com>
2025-12-09 07:38:03 +0800

    feat: 导数绘制图

---
+++ machinelearning/5chapter/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 定义函数y=sin(x)
+x = np.arange(0,6,0.1)
+y = np.sin(x)
+
+# 定义导函数
+y1 = np.cos(x)
+
+plt.plot(x,y,label="sin(x)")
+plt.plot(x,y1,label="cos(x)")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+
+
