3f7069af8c76d1c1bf38ec361ca13c59674568b4
dezhizhang <1541609448@qq.com>
2025-12-13 08:28:59 +0800

    feat:拟合回归线

--- machinelearning/2chapter/06fitting_test.py
+++ machinelearning/2chapter/06fitting_test.py
@@ -23,4 +23,33 @@ ax[2].scatter(x,y)
 
 plt.show()
 
+trainX,testX,trainY,testY = train_test_split(x,y,test_size=0.2,random_state=42)
+
+# 定义模型
+model = LinearRegression()
+
+# 第一次拟合
+x_train1 = trainX
+x_test1 = testX
+
+model.fit(x_train1,trainY)
+
+# 打印查看模型参数
+print(model.coef_)
+print(model.intercept_)
+
+#5. 预测结果计算误差
+y_pred1 = model.predict(x_test1)
+test_loss = mean_squared_error(testY,y_pred1)
+train_loss1 = mean_squared_error(trainY,model.predict(x_train1))
+
+# 画出拟合典线并写出训练误差与
+ax[0].plot(x,model.predict(x),'r')
+ax[0].text(-3,1,f"test loss: {test_loss:.4f}")
+ax[1].text(-3,1.3,f"train loss: {train_loss1:.4f}")
+plt.show()
+
+
+
+
 
