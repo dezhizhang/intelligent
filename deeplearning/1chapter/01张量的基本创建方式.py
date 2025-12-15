49ab38e85b9546b723f1efaf96b193c52057498a
dezhizhang <1541609448@qq.com>
2025-12-16 07:21:10 +0800

    feat:指定张量

--- deeplearning/1chapter/01张量的基本创建方式.py
+++ deeplearning/1chapter/01张量的基本创建方式.py
@@ -1,21 +1,6 @@
 import torch
 import numpy as np
 
-# def dm01():
-#     t1 = torch.tensor(10)
-#     print(f"t1:{t1} type:{type(t1)}")
-#
-#     print("*" * 30)
-#     data = [[1,2,3],[4,5,6]]
-#     t2 = torch.tensor(data)
-#     print(f"t2:{t2} type:{type(t2)}")
-#
-#     print("*" * 30)
-#
-#     data = np.random.randint(0,10,size=(2,3))
-#     t3 = torch.tensor(data)
-#     print(f"t3:{t3} type:{type(t3)}")
-
 def dm01():
     t1 = torch.tensor(10)
     print(f"t1:{t1} type:{type(t1)}")
