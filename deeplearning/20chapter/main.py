6489c843788ddb75dfa2a69423d1c3e69fb1c926
dezhizhang <1541609448@qq.com>
2025-12-16 06:48:52 +0800

    feat:张量计算

--- deeplearning/20chapter/main.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-t1 = torch.randint(1,10,(2,3,4))
-print(f"t1:{t1}")
-
-
--- deeplearning/29chapter/main.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-w = torch.tensor(10,requires_grad=True,dtype=torch.float32)
-
-loss = w ** 2 + 20
-
-print(f"开始权重初始值:{w} {loss} ")
