6c6a0a1c0c80141ce0497f1067e9a324ea2fe721
dezhizhang <1541609448@qq.com>
2025-12-18 07:09:39 +0800

    feat:张量的操作

---
+++ deeplearning/2chapter/06张量的形状操作.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def dm01():
+    t1 = torch.randint(1,10,size=(2,3))
+    print(f"t1:{t1} shape:{t1.shape}")
+
+    t2 = t1.reshape(1,6)
+    print(f"t2:{t2} shape:{t2.shape}")
+
+
+if __name__ == "__main__":
+    dm01()
+
