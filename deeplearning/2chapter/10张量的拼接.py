6f0a297e5e01b10f711dceebc5ccc2bc28a9c569
dezhizhang <1541609448@qq.com>
2026-01-04 20:30:18 +0800

    feat:自动微分模块

--- deeplearning/2chapter/10张量的拼接.py
+++ deeplearning/2chapter/10张量的拼接.py
@@ -1,12 +1,30 @@
-import  torch
+# import  torch
+#
+#
+# torch.manual_seed(0)
+#
+# t1 = torch.randint(1,10,(2,3))
+# t2 = torch.randint(1,10,(2,3))
+#
+# t = torch.stack([t1,t2],dim=0)
+# print(f"t:{t} shape:{t.shape}")
+
+# import  torch
+
+# t1 = torch.randint(1,10,(2,3))
+# t2 = torch.randint(1,10,(2,3))
+#
+# t = torch.cat([t1,t2],dim=0)
+# print(f"张量的拼接:{t} shape:{t.shape}")
 
 
-torch.manual_seed(0)
+import torch
 
 t1 = torch.randint(1,10,(2,3))
 t2 = torch.randint(1,10,(2,3))
 
-t = torch.stack([t1,t2],dim=0)
-print(f"t:{t} shape:{t.shape}")
+
+
+
 
 
--- deeplearning/3chapter/01自动微分.py
+++ deeplearning/3chapter/01自动微分.py
@@ -1,17 +1,14 @@
 import torch
 
-w = torch.tensor(10,requires_grad=True,dtype=torch.float)
+x = torch.tensor(10,requires_grad=True,dtype=torch.float)
 
-# 定义loss表示损失函数
-loss = 2 * w ** 2
+print("x->",x)
 
-# 打印梯度函数类型
-print(f"梯度函数类型:{type(loss.grad_fn)}")
+y = 2 *x **2
+print("y->",y)
 
-# 计算梯度。梯度=损失函数的导数
-loss.backward()
 
-# 权重公司
-w.data = w.data - 0.01 * w.grad
+print(y.grad_fn)
 
-print(f"更新后的权重:{w}")
+print("y.sum",y.sum())
+
