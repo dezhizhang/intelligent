95a9c25ae00ac281bea680c968fcc48fd1da1df2
dezhizhang <1541609448@qq.com>
2026-01-12 07:14:53 +0800

    feat:矩阵的行列式

---
+++ aimath/04chapter/01矩阵的转置.py
@@ -0,0 +1,51 @@
+# import numpy as np
+#
+# v1 = np.array([1,2,3,4,5,6]).reshape(3,2)
+# print(v1)
+#
+# r1 = np.linalg.matrix_rank(v1)
+# print(r1)
+
+
+# import numpy as np
+#
+# v1 = np.array([1,2,3,4,5,6]).reshape(3,3)
+# print(v1)
+#
+# r1 = np.linalg.matrix_rank(v1)
+# print(r1)
+
+# import numpy as np
+#
+# A = np.array([[2,3,4],[5,6,7],[1,2,1]])
+#
+# print(A)
+#
+# print(np.linalg.det(A))
+
+# import numpy as np
+#
+# A = np.array([[2,3,4],[5,6,7],[1,2,1]])
+#
+# print(A)
+#
+# print(np.linalg.det(A))
+
+import numpy as np
+
+A = np.array([[2,3,4],[5,6,7],[1,2,1]])
+
+print(np.linalg.det(A))
+
+
+
+
+
+
+
+
+
+
+
+
+
