b2bf009923382e1db49095c967a2431675dd72bd
dezhizhang <1541609448@qq.com>
2026-01-11 16:40:07 +0800

    feat:图表

---
+++ aimath/01chapter/main2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+heights = [102,105,110,114,120]
+
+args = [10,11,12,13,14]
+
+plt.bar(args,heights)
+plt.xlabel("age")
+plt.ylabel("height")
+plt.title("tom to age")
+plt.show()
+
