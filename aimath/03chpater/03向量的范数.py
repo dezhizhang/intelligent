32f59c993e33ea1812ad3ae67c992c5fec584273
dezhizhang <1541609448@qq.com>
2026-01-12 05:07:01 +0800

    feat:矩阵的入门

---
+++ aimath/03chpater/03向量的范数.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print(np.linalg.norm([1,2],1))
+print(np.linalg.norm([1,2],2))
---
+++ aimath/03chpater/05矩阵的入门.py
@@ -0,0 +1,61 @@
+# import numpy as np
+#
+# a = np.array([1,2,3])
+# b = np.array([7,8,9])
+#
+# c = a + b
+# print(c)
+# print(type(c))
+#
+
+# import numpy as np
+#
+# a = np.array([1,2,3])
+# b = np.array([7,8,9])
+#
+# print(a.dot(b))
+
+# import numpy as np
+#
+# a = np.array([1,2,3])
+# b = np.array([7,8,9])
+#
+# c = a.dot(b)
+# print(c)
+
+# import numpy as np
+#
+# a = np.array([1,2,3])
+# b = np.array([7,9,9])
+#
+# c = a.dot(b)
+# print(c)
+
+
+# import numpy as np
+#
+# a = np.array([3,-2,4,-1])
+# print(np.linalg.norm(a,ord=1))
+# print(np.linalg.norm(a,ord=2))
+# print(np.linalg.norm(a,ord=np.inf))
+#
+
+
+import numpy as np
+
+a = np.array([3,-2,4,-1])
+print(np.linalg.norm(a,ord=1))
+print(np.linalg.norm(a,ord=2))
+print(np.linalg.norm(a,ord=np.inf))
+
+
+
+
+
+
+
+
+
+
+
+
