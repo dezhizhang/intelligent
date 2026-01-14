5436d18f28e4ddd7608bc8bd6e1e4bdc228a326f
dezhizhang <1541609448@qq.com>
2026-01-15 06:35:53 +0800

    feat:添加日志系统

--- agent/app/main.py
+++ agent/app/main.py
@@ -1,11 +1,17 @@
 from code.config import get_settings
 from fastapi import FastAPI
+# from app.infrastructure.logging import setup_logging
+#
 
 
-# 加载配置信息
+# 1. 加载配置信息
 settings = get_settings()
 print(settings)
 
+# 2.初始化日志系统
+# setup_logging()
+# logger = logging.getLogger()
+# logger.info("测试")
 app = FastAPI()
 
 
