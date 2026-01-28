a047c36cc1e5d4932f5434b6e6e565be57068078
dezhizhang <1541609448@qq.com>
2026-01-29 06:18:46 +0800

    feat:添加日志信息

---
+++ knowledge/utils/logger_handler.py
@@ -0,0 +1,59 @@
+import logging
+import os
+import sys
+
+from utils.path_tool import get_abs_path
+
+# 日志保存的根目录
+LOG_ROOT = get_abs_path('logs')
+
+# 确保日志的目录存在
+os.makedirs(LOG_ROOT, exist_ok=True)
+
+# 日志的格式配置
+DEFAULT_LOG_FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s')
+
+
+def get_logger(
+        name: str = "agent",
+        console_level: int = logging.INFO,
+        file_level: int = logging.DEBUG,
+        log_file=None,
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(file_level)
+
+    # 避免重复添加handler
+    if logger.handlers:
+        return logger
+
+    # 控制台handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(console_level)
+    console_handler.setFormatter(DEFAULT_LOG_FORMAT)
+
+    logger.addHandler(console_handler)
+
+    # 文件handler
+    if not log_file:
+        log_file = os.path.join(LOG_ROOT, "agent.log")
+
+    file_handler = logging.FileHandler(log_file,encoding="utf-8")
+    file_handler.setLevel(file_level)
+    file_handler.setFormatter(DEFAULT_LOG_FORMAT)
+    logger.addHandler(file_handler)
+
+    return logger
+
+# 快捷获取日志器
+logger = get_logger(name="agent")
+
+
+if __name__ == '__main__':
+    logger = get_logger(name="agent")
+
+
+
+
+
+
