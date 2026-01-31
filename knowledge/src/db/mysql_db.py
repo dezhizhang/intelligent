fdfb33cc774fdaef295ccbf13f58f43e685a6a80
dezhizhang <1541609448@qq.com>
2026-01-31 19:58:32 +0800

    feat:获取会话session

--- knowledge/src/db/mysql_db.py
+++ knowledge/src/db/mysql_db.py
@@ -1,11 +1,13 @@
 import os
-from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine
+from typing import AsyncIterator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm.session import sessionmaker
 
 
 def get_db_url() -> str:
     """获取数据连接url"""
     return os.environ["MYSQL_DATABASE_URL"]
+
 
 engine = create_async_engine(
     get_db_url(),
@@ -20,3 +22,16 @@ async_session = sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
+
+
+async def get_db() -> AsyncIterator[AsyncSession, None]:
+    try:
+        db = await async_session()
+        yield db
+    except Exception as ex:
+        print(f"数据库操作错误:{ex}")
+        if db and db.in_transaction():
+            await db.rollback()
+    finally:
+        if db:
+            await db.close()
