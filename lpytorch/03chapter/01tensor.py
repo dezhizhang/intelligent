ff4c478feae8df38a6aac9df34d0f77f0445a30c
dezhizhang <1541609448@qq.com>
2026-02-01 14:22:13 +0800

    feat:张量

---
+++ lpytorch/03chapter/01tensor.py
@@ -0,0 +1,99 @@
+# import torch
+#
+# a = torch.tensor([[1,2],[3,4]])
+# print(a)
+# print(a.type())
+# import torch
+#
+# # 指定形状的tensor
+# a = torch.Tensor(2,3)
+# print(a)
+# print(a.type())
+import torch
+
+# # 全1的tensor
+# import torch
+# a = torch.ones(2,2)
+# print(a)
+# print(a.type())
+
+# import torch
+# a = torch.ones(2,2)
+# print(a)
+# print(a.type())
+
+# 全0的tensor
+# a = torch.zeros(2,2)
+# print(a)
+# print(a.type())
+
+# 全0的tensor
+# a = torch.zeros(2,2)
+# print(a)
+
+# a = torch.zeros(2,2)
+# print(a)
+
+
+# a = torch.zeros(2,2)
+# print(a)
+# 随机
+# a = torch.rand(2,2)
+# print(a)
+# print(a.type())
+
+# a = torch.rand(2,2)
+# print(a)
+# print(a.type())
+
+
+# 对应
+# a = torch.normal(mean=torch.rand(5),std=torch.rand(5))
+# print(a)
+# print(a.type())
+
+# a = torch.arange(0,11,3)
+# print(a)
+# print(a.type())
+
+# a = torch.arange(0,11,3)
+# print(a)
+# print(a.type())
+
+# a = torch.linspace(2,10,4)
+# print(a)
+# print(a.type())
+
+# a = torch.linspace(2,10,4)
+# print(a)
+# print(a.type())
+
+# a = torch.linspace(2,10,4)
+# print(a)
+# print(a.type())
+
+
+a = torch.linspace(2,10,4)
+print(a)
+print(a.type())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lpytorch/03chapter/02attribute.py
@@ -0,0 +1,6 @@
+import torch
+device = torch.device("cpu")
+a = torch.tensor([2,2],device=device)
+print(a)
+
+
