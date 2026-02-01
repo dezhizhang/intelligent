b3805daebfd128b85579cf387f7aedc66a9e797e
dezhizhang <1541609448@qq.com>
2026-02-01 16:35:02 +0800

    feat:随机抽样

---
+++ lpytorch/03chapter/06compare.py
@@ -0,0 +1,20 @@
+
+# import torch
+#
+# a = torch.rand(2,3)
+# print(torch.isfinite(a))
+# print(torch.isfinite(a/0))
+# print(torch.isinf(a))
+# print(torch.isnan(a))
+
+import torch
+
+a = torch.rand(2,3)
+print(torch.isfinite(a))
+print(torch.isinf(a))
+print(torch.isnan(a))
+
+
+
+
+
---
+++ lpytorch/03chapter/08static.py
@@ -0,0 +1,39 @@
+# import torch
+#
+# a = torch.rand(2,2)
+# print(a)
+# print(torch.mean(a))
+# print(torch.sum(a))
+# print(torch.prod(a))
+
+# import torch
+#
+# a = torch.rand(2,2)
+# print(a)
+#
+# print(torch.mean(a))
+# print(torch.sum(a))
+# print(torch.prod(a))
+
+# import torch
+# a = torch.rand(2,2)
+# print(a)
+#
+# print(torch.mean(a))
+# print(torch.sum(a))
+# print(torch.prod(a))
+
+# import torch
+# a = torch.randint(0,10,[2,10])
+# print(torch.bincount(a))
+
+import torch
+a = torch.randint(0,10,[2,10])
+print(torch.bincount(a))
+
+
+
+
+
+
+
