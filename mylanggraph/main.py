740191b39faae89ca045e00f5ecebf75eea285f3
dezhizhang <1541609448@qq.com>
2026-02-08 21:47:37 +0800

    feat:流式输出

--- mylanggraph/main.py
+++ mylanggraph/main.py
@@ -1,77 +1,25 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.tools import tool
-from langgraph.graph import MessagesState,START
-from langgraph.prebuilt import ToolNode
-from langgraph.graph import END,StateGraph
-from langgraph.checkpoint.memory import MemorySaver
+from bs4 import BeautifulSoup as Soup
+from langchain_community.document_loaders.recursive_url_loader import  RecursiveUrlLoader
 
+url = "https://python.langchain.com/docs/concepts/lcel/"
 
-@tool
-def play_song_on_qq(song:str):
-    """在qq音乐上播放歌曲"""
-    return f"成功在QQ音乐上播放了{song}!"
-
-@tool
-def play_song_on_163(song:str):
-    """在网易云上播放歌典"""
-    return f"成功在网易云上播放了{song}!"
-
-tools = [play_song_on_163,play_song_on_qq]
-
-tool_node = ToolNode(tools)
-
-
-
-llm = ChatOpenAI(
-    api_key="sk-zUDelHgZPjOX4eP3tnTcVXRC9cgA8yerufoOMyeM7V9Hx9GM",
-    base_url="https://poloai.top/v1",
-    temperature=0,
-    streaming=True,
+loader = RecursiveUrlLoader(
+    url=url,
+    max_depth=30,
+    extractor= lambda x:Soup(x,"html.parser").text
 )
 
-model = llm.bind_tools(tools,parallel_tool_calls=False)
+docs = loader.load()
+d_sorted = sorted(docs, key=lambda x: x.metadata['source'])
+d_reversed = list(reversed(d_sorted))
 
-def should_continue(state):
-    messages = state["messages"]
-    last_message = messages[-1]
-    if not last_message.tool_calls:
-        return "end"
-    else:
-        return "continue"
-
-def call_model(state):
-    messages = state["messages"]
-    response = llm.invoke(messages)
-    return {"messages": response}
-
-# 定义一个新图
-workflow = StateGraph(MessagesState)
-
-workflow.add_node("agent",call_model)
-workflow.add_node("action",tool_node)
-
-
-workflow.add_edge(START,"agent")
-
-workflow.add_conditional_edges(
-    "agent",
-    should_continue,
-    {
-        "continue":"action",
-        "end":END,
-    }
+concatenated_content = "\n\n\n --- \n\n\n".join(
+    [doc.page_content for doc in d_reversed]
 )
 
-workflow.add_edge("action","agent")
+print(concatenated_content)
 
-# 设置内存
-memory = MemorySaver()
-
-app = workflow.compile(checkpointer=memory)
-
-
-
-
+# 设置大模型
 
 
 
