61a7f9d88d1afe2fe3841293c32f4d1b9d6859e6
dezhizhang <1541609448@qq.com>
2026-02-11 22:59:15 +0800

    feat:算数运算

--- math/6chapter/02维度与形状.py
+++ math/6chapter/02维度与形状.py
@@ -1,10 +1,37 @@
+# import numpy as np
+#
+# arr1 = np.arange(1,5).reshape(2,2)
+# arr2 = np.arange(6,10).reshape(2,2)
+#
+# arr = np.concatenate((arr1,arr2),axis=1)
+# print(arr)
+
+# import numpy as np
+#
+# a = np.arange(1,7).reshape((2,3))
+# b = np.arange(7,13).reshape((2,3))
+# c = np.arange(13,19).reshape((2,3))
+# d = np.arange(19,25).reshape((2,3))
+#
+# t = np.stack([a,b,c,d],axis=0)
+# print(f"axis=0 shape:{t.shape}\n")
+
 import numpy as np
 
-arr1 = np.arange(1,5).reshape(2,2)
-arr2 = np.arange(6,10).reshape(2,2)
 
-arr = np.concatenate((arr1,arr2),axis=1)
-print(arr)
+a = np.arange(1,7).reshape((2,3))
+b = np.arange(7,13).reshape((2,3))
+c = np.arange(13,19).reshape((2,3))
+d = np.arange(19,25).reshape((2,3))
+
+t = np.stack([a,b,c,d],axis=0)
+print(f"axis=0 shape:{t.shape}")
+
+
+
+
+
+
 
 
 
---
+++ math/6chapter/03广播机制.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([[2],[3]])
+b = np.arange(3,9).reshape(2,3)
+
+print(f"{a.shape},{b.shape}")
+
+
+
