320df2dc1cd55367f26cf15c88b16dbb64317de8
dezhizhang <1541609448@qq.com>
2026-03-13 07:29:18 +0800

    feat:理合特征

--- machinelearning/2chapter/03鸢尾花案例.py
+++ machinelearning/2chapter/03鸢尾花案例.py
@@ -11,7 +11,8 @@
 from sklearn.datasets import load_iris
 import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt
+
+
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler  # KNN算法 分类对像
---
+++ machinelearning/4chapter/03过拟合欠拟合.py
@@ -0,0 +1,44 @@
+"""
+案例:
+    演示： 欠拟合，正好拟合，过拟合，L1正则化
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from  sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_squared_error,root_mean_squared_error,mean_absolute_error
+from sklearn.linear_model import Ridge,RidgeCV
+
+
+def dm01_under_fitting():
+    # 1. 随机生成x轴100个数据，模拟：特征
+    x = np.random.uniform(-3,3,100)
+    # 2. 基于x轴值通过线生公式，生成y轴100个数据
+    y = 0.5 * x * x **2 + x + 2 + np.random.normal(0,1,100)
+
+    # 3. 数据预处理
+    X = x.reshape(-1,1)
+
+    # 4. 训练模型
+    estimator = LinearRegression()
+    estimator.fit(X,y)
+
+    # 5. 模型预测
+    y_pre = estimator.predict(X)
+
+    # 6. 模型评估
+    print(f"均方误差:{mean_squared_error(y,y_pre)}")
+
+    # 7. 绘图
+    plt.scatter(x,y)
+    plt.plot(X,y_pre,color='red')
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    dm01_under_fitting()
